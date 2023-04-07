b20df2fc01dabbab3aaa273440a669919b4ca830
Harshitha Andoji <95291532+Andoji@users.noreply.github.com>
2023-04-07 18:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use('/questions', questionRoutes)
 app.use('/answer', answerRoutes)
 const PORT = process.env.PORT || 5000
 
-const CONNECTION_URL = process.env.CONNECTION_URL
+const DATABASE_URL = process.env.CONNECTION_URL
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
       .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
       .catch((err) => console.log(err.message))
